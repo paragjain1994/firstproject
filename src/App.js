0a321ad4a22aa11376123d17c5e2062a3e31f496
parag <paragjain528@gmail.com>
2022-11-12 17:03:25 +0530

    AddUser component with form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import AddUser from './components/AddUser';
 
 function App() {
   return (
     <div>
 <h1>React first project</h1>
+<AddUser></AddUser>
     </div>
   );
 }
