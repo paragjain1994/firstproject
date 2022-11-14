671a769c4f80e61d1005330023a30d7b7f3ea2de
parag <paragjain528@gmail.com>
2022-11-14 13:48:32 +0530

    styling to the UsersList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,19 @@
 import React from "react";
+import classes from "./UsersList.module.css";
+import Card from "./Card";
 
 const UsersList = (props) => {
   return (
-    <ul>
-      {props.users.map((user) => (
-        <li key={user.id}>
-          {user.name} ({user.age} years old)
-        </li>
-      ))}
-    </ul>
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
   );
 };
 
-export default UsersList;
+export default UsersList; 
